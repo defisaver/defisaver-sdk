77ba6d92aaf0d50a92de5e574af72b75c189965f
Nenad Palinkasevic <nesa993@gmail.com>
2020-12-05 21:47:59 +0100

    Export actions

--- src/actions/maker/index.js
+++ src/actions/maker/index.js
@@ -1,5 +1,10 @@
 const MakerOpenVaultAction = require('./MakerOpenVaultAction');
+const MakerGenerateAction = require('./MakerGenerateAction');
+const MakerSupplyAction = require('./MakerSupplyAction');
+
 
 module.exports = {
   MakerOpenVaultAction,
+  MakerGenerateAction,
+  MakerSupplyAction,
 };
