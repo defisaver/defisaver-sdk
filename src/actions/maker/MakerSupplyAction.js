3f898e68b6f8998ae57f71e72a1168ad53bf9ef0
Nenad Palinkasevic <nesa993@gmail.com>
2020-12-05 21:33:48 +0100

    MakerSupplyAction

---
+++ src/actions/maker/MakerSupplyAction.js
@@ -0,0 +1,18 @@
+const Action = require("../../Action");
+
+/**
+ * MakerSupplyAction - Supply token to a Vault
+ */
+class MakerSupplyAction extends Action {
+  /**
+   * @param vaultId {String}
+   * @param amount {String}
+   * @param joinAddr {String}
+   * @param from {String}
+   */
+  constructor(vaultId, amount, joinAddr, from) {
+    super('McdGenerate', '0x0', ['uint256','uint256','address','address'], [...arguments]);
+  }
+}
+
+module.exports = MakerSupplyAction;
