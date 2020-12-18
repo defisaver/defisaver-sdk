5e064419653b954f4514b91ad73b1402d00a11e0
Nikola Klipa <klipa.nikola@gmail.com>
2020-12-18 12:33:34 +0800

    Add AaveCustomFlashLoanV2Action

--- src/actions/flashloan/index.js
+++ src/actions/flashloan/index.js
@@ -1,9 +1,11 @@
 const AaveFlashLoanAction = require('./AaveFlashLoanAction');
 const DyDxFlashLoanAction = require('./DyDxFlashLoanAction');
 const AaveFlashLoanV2Action = require('./AaveFlashLoanV2Action');
+const AaveCustomFlashLoanV2Action = require('./AaveCustomFlashLoanV2Action');
 
 module.exports = {
   AaveFlashLoanAction,
   DyDxFlashLoanAction,
   AaveFlashLoanV2Action,
+  AaveCustomFlashLoanV2Action,
 };
