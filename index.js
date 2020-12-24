4a723285db69b410ccb5ce2f1b85a0a776ab4c48
Nenad Palinkasevic <nesa993@gmail.com>
2020-12-24 23:58:09 +0100

    Export mainnet addresses

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const Action = require('./src/Action');
 const Recipe = require('./src/Recipe');
 
 const actions = require('./src/actions/');
+const { actionAddresses } = require('./src/addresses');
 
 module.exports = {
   Action,
   Recipe,
   actions,
+  actionAddresses
 }
