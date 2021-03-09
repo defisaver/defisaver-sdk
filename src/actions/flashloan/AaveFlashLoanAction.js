6b8a609d103a9557dae21c6ff61f24f94d5c8816
Nenad Palinkasevic <nesa993@gmail.com>
2021-03-09 13:19:06 +0100

    Aave fl new contract inputs

--- src/actions/flashloan/AaveFlashLoanAction.js
+++ src/actions/flashloan/AaveFlashLoanAction.js
@@ -8,9 +8,11 @@ class AaveFlashLoanAction extends Action {
   /**
    * @param loanAmount {string}
    * @param tokenAddr {EthAddress}
+   * @param flParamGetterAddr {EthAddress}
+   * @param flParamGetterData {bytes}
    */
-  constructor(loanAmount, tokenAddr) {
-    super('FLAave', getAddr('FLAave'), ['uint256','address', 'uint256'], [loanAmount, tokenAddr, 0]);
+  constructor(loanAmount, tokenAddr, flParamGetterAddr, flParamGetterData) {
+    super('FLAave', getAddr('FLAave'), ['uint256','address', 'address', 'bytes','uint256'], [loanAmount, tokenAddr, flParamGetterAddr, flParamGetterData, 0]);
   }
 }
 
