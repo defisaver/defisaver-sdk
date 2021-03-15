00aec42c99db33bf6b643576d4c23b628a9c8b45
Nenad Palinkasevic <nesa993@gmail.com>
2021-03-15 12:09:12 +0100

    Remove unused actions

--- src/actions/flashloan/index.js
+++ src/actions/flashloan/index.js
@@ -1,17 +1,11 @@
 const DyDxFlashLoanAction = require('./DyDxFlashLoanAction');
 const DyDxFlashLoanPaybackAction = require('./DyDxFlashLoanPaybackAction');
-const AaveFlashLoanAction = require('./AaveFlashLoanAction');
-const AaveFlashLoanPaybackAction = require('./AaveFlashLoanPaybackAction');
 const AaveV2FlashLoanAction = require('./AaveV2FlashLoanAction');
 const AaveV2FlashLoanPaybackAction = require('./AaveV2FlashLoanPaybackAction');
-const AaveCustomFlashLoanV2Action = require('./AaveCustomFlashLoanV2Action');
 
 module.exports = {
   DyDxFlashLoanAction,
   DyDxFlashLoanPaybackAction,
-  AaveFlashLoanAction,
-  AaveFlashLoanPaybackAction,
   AaveV2FlashLoanAction,
   AaveV2FlashLoanPaybackAction,
-  AaveCustomFlashLoanV2Action,
 };
