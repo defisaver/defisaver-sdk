6fe051a3039e15c0b6c8f1c6bc6aca105de3792e
Nenad Palinkasevic <nesa993@gmail.com>
2021-03-18 11:06:12 +0100

    Add PullToken action

--- src/actions/basic/index.js
+++ src/actions/basic/index.js
@@ -1,9 +1,11 @@
 const SellAction = require('./SellAction');
 const BuyAction = require('./BuyAction');
 const SendTokenAction = require('./SendTokenAction');
+const PullTokenAction = require('./PullTokenAction');
 
 module.exports = {
   SellAction,
   BuyAction,
   SendTokenAction,
+  PullTokenAction
 };
