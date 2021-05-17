d8cee5265648998d92be9144b03c983c3514b40a
sterlu <sterlu.sd@gmail.com>
2021-05-17 12:58:52 +0200

    Move Uniswap V3 actions to separate folder for consistency; Lint & fix import errors

--- src/actions/uniswap/index.js
+++ src/actions/uniswap/index.js
@@ -1,15 +1,7 @@
 const UniswapSupplyAction = require('./UniswapSupplyAction');
 const UniswapWithdrawAction = require('./UniswapWithdrawAction');
-const UniSupplyV3Action = require('./v3/UniSupplyV3Action');
-const UniWithdrawV3Action = require('./v3/UniWithdrawV3Action');
-const UniCollectV3Action = require('./v3/UniCollectV3Action');
-const UniMintV3Action = require('./v3/UniMintV3Action');
 
 module.exports = {
   UniswapSupplyAction,
   UniswapWithdrawAction,
-  UniSupplyV3Action,
-  UniWithdrawV3Action,
-  UniCollectV3Action,
-  UniMintV3Action,
 };
