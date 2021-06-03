18de8c0af6e72004d0121c2c5ace1cda4cb2c3e6
Nenad Palinkasevic <nesa993@gmail.com>
2021-06-03 16:20:36 +0200

    Change GasFeeAction inputs

--- src/actions/basic/GasFeeAction.js
+++ src/actions/basic/GasFeeAction.js
@@ -5,17 +5,12 @@ class GasFeeAction extends Action {
   /**
    * @param gasStart {string} Always 0 will be inject value
    * @param feeToken {string} Address of the token we are taken the fee in
-   * @param oracleAddr {string} Chainlink oracle addr
-   * @param actionId {string} Id of the fee taking action
-   * @param actionData {string} Params of the fee taking action
+   * @param payNow {bool} If the action will pay gas cost from DSProxy now or later
    */
-  constructor(gasStart, feeToken, oracleAddr, actionId, actionData) {
-    super("GasFeeTaker", getAddr("GasFeeTaker"), ["uint256","address","address","bytes32","bytes"], [...arguments]);
+  constructor(gasStart, feeToken, payNow) {
+    super("GasFeeTaker", getAddr("GasFeeTaker"), ["uint256","address","bool"], [...arguments]);
   }
 
-  async getEthValue() {
-    return this.args[0];
-  }
 }
 
 module.exports = GasFeeAction;
