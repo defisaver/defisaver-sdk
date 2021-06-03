f23e03fd9a72492823d4dd9d6440b502c48550c9
Nenad Palinkasevic <nesa993@gmail.com>
2021-06-03 17:47:13 +0200

    Add sub action

---
+++ src/actions/basic/SubInputsAction.js
@@ -0,0 +1,21 @@
+const Action = require("../../Action");
+const { getAddr } = require('../../addresses.js');
+
+/**
+ * Subs 2 inputs/return values
+ */
+class SubInputsAction extends Action {
+  constructor(a, b) {
+    super(
+      'SubInputs',
+      getAddr('SubInputs'),
+      [
+        "uint",
+        "uint",
+      ],
+      [...arguments]
+    );
+  }
+}
+
+module.exports = SubInputsAction;
