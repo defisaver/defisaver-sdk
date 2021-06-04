f703d81c1860818be6b604ad825b88ae8d5f6145
majkic99 <60391069+majkic99@users.noreply.github.com>
2021-06-04 16:51:21 +0200

    fix : removed action that works with pure eth

--- src/actions/lido/LidoStakeAction.js
+++ src/actions/lido/LidoStakeAction.js
@@ -3,23 +3,25 @@ const {getAssetInfoByAddress} = require("@defisaver/tokens");
 const { getAddr } = require('../../addresses.js');
 
 /**
- * LidoStakeAction - Stakes ETH sent on Lido contract, receives stETH in return
+ * LidoStakeAction - Receives WETH, transforms it to ETH then sends it to Lido staking contract receiving stETH in return
  */
 class LidoStakeAction extends Action {
   /**
-   * @param ethAmount {string} amount of ETH that will be staked to Lido contract
+   * @param amount {string} amount of WETH to pull and stake
+   * @param from {EthAddress} WETH will be taken from this address
    * @param to {ETHAddress} stETH will be sent to this address
    */
-  constructor(ethAmount, to) {
-    super('LidoStake', getAddr('LidoStake'), [['uint256', 'address']], [[ethAmount, to]]);
+  constructor(amount, from, to, toWrap) {
+    super('LidoStake', getAddr('LidoStake'), [['uint256','address', 'address']], [[amount, from, to]]);
     this.mappableArgs = [
       this.args[0][0],
       this.args[0][1],
+      this.args[0][2],
     ];
   }
   
   async getAssetsToApprove() {
-    return [];
+    return [{asset: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', owner: this.args[0][1]}];
   }
 }
 
--- src/actions/lido/index.js
+++ src/actions/lido/index.js
@@ -1,7 +1,4 @@
 const LidoStakeAction = require('./LidoStakeAction');
-const LidoWETHStakeAction = require('./LidoWETHStakeAction');
-
 module.exports = {
     LidoStakeAction,
-    LidoWETHStakeAction,
 }
