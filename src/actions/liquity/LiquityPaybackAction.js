abf047565aacc04d827964943c149fbe1557acb4
kjaredic <kjaredic@protonmail.com>
2021-06-06 21:18:37 +0200

    lint all liquity tests

--- src/actions/liquity/LiquityPaybackAction.js
+++ src/actions/liquity/LiquityPaybackAction.js
@@ -1,33 +1,32 @@
-const Action = require("../../Action");
-const { getAssetInfo } = require("@defisaver/tokens");
-const {requireAddress} = require("../../utils/general");
-const { getAddr } = require('../../addresses.js');
+const { getAssetInfo } = require('@defisaver/tokens');
+const Action = require('../../Action');
+const { requireAddress } = require('../../utils/general');
+const { getAddr } = require('../../addresses');
 
 /**
  * LiquityPaybackAction - Repays LUSD to the trove
  */
 class LiquityPaybackAction extends Action {
-  /**
+    /**
    * @param lusdAmount Amount of LUSD tokens to repay
    * @param from Address where the tokens are pulled from
    */
-  constructor(lusdAmount, from, upperHint, lowerHint) {
-    requireAddress(from);
-    super('LiquityPayback',
-        getAddr('LiquityPayback'),
-        [['uint256','address', 'address','address']],
-        [[...arguments]]
-    );
+    constructor(lusdAmount, from, upperHint, lowerHint) {
+        requireAddress(from);
+        super('LiquityPayback',
+            getAddr('LiquityPayback'),
+            [['uint256', 'address', 'address', 'address']],
+            [[...arguments]]);
 
-    this.mappableArgs = [
-      this.args[0][0],
-      this.args[0][1]
-    ];
-  }
+        this.mappableArgs = [
+            this.args[0][0],
+            this.args[0][1],
+        ];
+    }
 
-  async getAssetsToApprove() {
-    return [{asset: getAssetInfo('LUSD').address, owner: this.args[0][1]}];
-  }
+    async getAssetsToApprove() {
+        return [{ asset: getAssetInfo('LUSD').address, owner: this.args[0][1] }];
+    }
 }
 
 module.exports = LiquityPaybackAction;
--- src/actions/liquity/LiquitySupplyAction.js
+++ src/actions/liquity/LiquitySupplyAction.js
@@ -1,33 +1,32 @@
-const Action = require("../../Action");
-const { getAssetInfo } = require("@defisaver/tokens");
-const {requireAddress} = require("../../utils/general");
-const { getAddr } = require('../../addresses.js');
+const { getAssetInfo } = require('@defisaver/tokens');
+const Action = require('../../Action');
+const { requireAddress } = require('../../utils/general');
+const { getAddr } = require('../../addresses');
 
 /**
  * LiquitySupplyAction - Supplies collateral to the trove
  */
 class LiquitySupplyAction extends Action {
-  /**
+    /**
    * @param collAmount Amount of WETH tokens to supply
    * @param from Address where the tokens are pulled from
    */
-  constructor(collAmount, from, upperHint, lowerHint) {
-    requireAddress(from);
-    super('LiquitySupply',
-        getAddr('LiquitySupply'),
-        [['uint256','address', 'address','address']],
-        [[...arguments]]
-    );
+    constructor(collAmount, from, upperHint, lowerHint) {
+        requireAddress(from);
+        super('LiquitySupply',
+            getAddr('LiquitySupply'),
+            [['uint256', 'address', 'address', 'address']],
+            [[...arguments]]);
 
-    this.mappableArgs = [
-      this.args[0][0],
-      this.args[0][1]
-    ];
-  }
+        this.mappableArgs = [
+            this.args[0][0],
+            this.args[0][1],
+        ];
+    }
 
-  async getAssetsToApprove() {
-    return [{asset: getAssetInfo('WETH').address, owner: this.args[0][1]}];
-  }
+    async getAssetsToApprove() {
+        return [{ asset: getAssetInfo('WETH').address, owner: this.args[0][1] }];
+    }
 }
 
 module.exports = LiquitySupplyAction;
