fc2de334a330a46355a4a9fb781c04a60a04735f
Nenad Palinkasevic <nesa993@gmail.com>
2021-06-09 14:50:10 +0200

    Add checkers action

---
+++ src/actions/checkers/MakerRatioCheckAction.js
@@ -0,0 +1,25 @@
+const Action = require("../../Action");
+const {getAddr} = require("../../addresses.js");
+
+/**
+ *
+ */
+class MakerRatioCheckAction extends Action {
+  /**
+   * @param targetRatio {string} The ratio user want to be at
+   * @param vaultId {string} Id of the vault
+   * @param nextPrice {string} Next maker oracle price if needed
+   */
+  constructor(targetRatio, vaultId, nextPrice) {
+    super("McdRatioCheck", getAddr("McdRatioCheck"), [["uint256", "uint256", "uint256"]], [...arguments]);
+
+    this.mappableArgs = [
+      this.args[0][1],
+      this.args[0][2],
+      this.args[0][3],
+    ];
+  }
+
+}
+
+module.exports = MakerRatioCheckAction;
