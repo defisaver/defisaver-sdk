232401cc45475ba70914c9ae8b21563322279ef9
majkic99 <vrnebojsamajkic@gmail.com>
2021-08-10 13:36:50 +0200

    add deployed contract address

--- src/addresses.js
+++ src/addresses.js
@@ -95,6 +95,7 @@ const actionAddresses = {
   // balancer
   'BalancerV2Supply': '0xD78E5D95A28a67F7851b0a94505790813A92E405',
   'BalancerV2Withdraw': '0xCcf4b96407BEF25D7df1c95045CCF64950e73E97',
+  'BalancerV2Claim': '0x259Ae83567858B7960d2De0D00F3717a764aD73B',
 };
 
 const otherAddresses = {
