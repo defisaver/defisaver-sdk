64244b8a7767304999595075e0acf62d475fa4a5
majkic99 <vrnebojsamajkic@gmail.com>
2021-08-18 11:54:23 +0200

    add univ3 trigger

---
+++ src/triggers/UniV3CurrentTickTrigger.js
@@ -0,0 +1,15 @@
+const Action = require("../Action");
+const {getAddr} = require("../addresses.js");
+
+class UniV3CurrentTickTrigger extends Action {
+
+  constructor(tokenId, state) {
+    super("UniV3CurrentTickTrigger", getAddr("UniV3CurrentTickTrigger"), [["uint256", "uint8"]], [[...arguments]]);
+  }
+
+  async getEthValue() {
+    return this.args[0];
+  }
+}
+
+module.exports = MakerRatioTrigger;
