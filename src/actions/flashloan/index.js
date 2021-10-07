f3f3be4b61cf09a233dc00cc6c46df5180979e27
kjaredic <kjaredic@protonmail.com>
2021-10-07 14:20:31 +0200

    feat: add Maker FL action

--- src/actions/flashloan/index.js
+++ src/actions/flashloan/index.js
@@ -2,10 +2,12 @@ const DyDxFlashLoanAction = require('./DyDxFlashLoanAction');
 const DyDxFlashLoanPaybackAction = require('./DyDxFlashLoanPaybackAction');
 const AaveV2FlashLoanAction = require('./AaveV2FlashLoanAction');
 const AaveV2FlashLoanPaybackAction = require('./AaveV2FlashLoanPaybackAction');
+const MakerFlashLoanAction = require('./MakerFlashLoanAction');
 
 module.exports = {
   DyDxFlashLoanAction,
   DyDxFlashLoanPaybackAction,
   AaveV2FlashLoanAction,
   AaveV2FlashLoanPaybackAction,
+  MakerFlashLoanAction,
 };
