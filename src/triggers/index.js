1e514d21eb2a531a071a42c1eaddb67aca2a2785
majkic99 <vrnebojsamajkic@gmail.com>
2021-10-12 11:38:19 +0200

    add comp ratio trigger

--- src/triggers/index.js
+++ src/triggers/index.js
@@ -3,6 +3,7 @@ const ChainLinkPriceTrigger = require('./ChainLinkPriceTrigger');
 const UniV3CurrentTickTrigger = require('./UniV3CurrentTickTrigger');
 const TimestampTrigger = require('./TimestampTrigger');
 const GasPriceTrigger = require('./GasPriceTrigger');
+const CompoundRatioTrigger = require('./CompoundRatioTrigger');
 
 module.exports = {
     MakerRatioTrigger,
@@ -10,4 +11,5 @@ module.exports = {
     UniV3CurrentTickTrigger,
     TimestampTrigger,
     GasPriceTrigger,
+    CompoundRatioTrigger,
 }
