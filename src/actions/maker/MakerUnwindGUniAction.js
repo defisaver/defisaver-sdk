a75dab7aacc77965725db5965088cdeca5707399
majkic99 <vrnebojsamajkic@gmail.com>
2021-11-19 11:24:23 +0100

    add natspec

--- src/actions/maker/MakerUnwindGUniAction.js
+++ src/actions/maker/MakerUnwindGUniAction.js
@@ -2,13 +2,18 @@ const Action = require("@defisaver/sdk/src/Action");
 const { getAddr } = require('@defisaver/sdk/src/addresses');
 
 /**
- * MakerUnwindGUni
+ * MakerUnwindGUniAction
  */
 class MakerUnwindGUniAction extends Action {
   /**
+   * @param daiAmount {min amount of dai to receive back from unwinding}
+   * @param mcdManager {EthAddress}
+   * @param vaultId {VaultId}
+   * @param mcdManager {EthAddress}
+   * @param sendDaiTo {Address to which to send all received dai}
    */
-  constructor(daiAmount, mcdManager, vaultId, sendDaiTo) {
-    super('McdUnwindGUni', getAddr('McdUnwindGUni'), [['uint256', 'address', 'uint256', 'address']], [[daiAmount, mcdManager, vaultId, sendDaiTo]] );
+  constructor(daiAmount, mcdManager, vaultId, guniLevAddr, sendDaiTo) {
+    super('McdUnwindGUni', getAddr('McdUnwindGUni'), [['uint256', 'address', 'uint256', 'address', 'address']], [[daiAmount, mcdManager, vaultId, guniLevAddr, sendDaiTo]] );
   }
 }
 
--- src/actions/maker/MakerWindGUniAction.js
+++ src/actions/maker/MakerWindGUniAction.js
@@ -2,13 +2,21 @@ const Action = require("@defisaver/sdk/src/Action");
 const { getAddr } = require('@defisaver/sdk/src/addresses');
 
 /**
- * MakerWindGUni
+ * MakerWindGUniAction
  */
 class MakerWindGUniAction extends Action {
   /**
+   * @param daiAmount {dai amount to pull to proxy}
+   * @param from {address from which to pull dai from}
+   * @param principal {dai amount to use as principal for leverage}
+   * @param minWalletDai {amount of usdc+dai you want to get back to wallet after this action}
+   * @param vaultId {VaultId, 0 if there isn't already opened GUNI vault, to create one}
+   * @param mcdManager {EthAddress}
+   * @param joinAddr {EthAddress}
+   * @param guniLevAddr {EthAddress}
    */
-  constructor(daiAmount, from, principal, minWalletDai, vaultId, mcdManager, joinAddr) {
-    super('McdWindGUni', getAddr('McdWindGUni'), [['uint256','address', 'uint256', 'uint256', 'uint256', 'address', 'address']], [[daiAmount, from, principal, minWalletDai, vaultId, mcdManager, joinAddr]] );
+  constructor(daiAmount, from, principal, minWalletDai, vaultId, mcdManager, joinAddr, guniLevAddr) {
+    super('McdWindGUni', getAddr('McdWindGUni'), [['uint256','address', 'uint256', 'uint256', 'uint256', 'address', 'address', 'address']], [[daiAmount, from, principal, minWalletDai, vaultId, mcdManager, joinAddr, guniLevAddr]] );
   }
   async getAssetsToApprove() {
     return [{asset: getAssetInfo('DAI').address, owner: this.args[0][1]}];
