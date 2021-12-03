d6d90901b9bd476e411fc4c23e153ed8e1da117e
Nenad Palinkasevic <nesa993@gmail.com>
2021-12-03 13:41:39 +0100

    Update maker ratio check

--- src/actions/checkers/MakerRatioCheckAction.js
+++ src/actions/checkers/MakerRatioCheckAction.js
@@ -2,24 +2,28 @@ const Action = require("../../Action");
 const {getAddr} = require("../../addresses.js");
 
 /**
- *
+ * MakerRatioCheckAction - Checks mcd ratio at end of all actions
  */
 class MakerRatioCheckAction extends Action {
   /**
+   * @param ratioState {uint8} If it should lower/higher
+   * @param checkTarget {}
    * @param targetRatio {string} The ratio user want to be at
    * @param vaultId {string} Id of the vault
    * @param nextPrice {string} Next maker oracle price if needed
    */
-  constructor(targetRatio, vaultId, nextPrice) {
-    super("McdRatioCheck", getAddr("McdRatioCheck"), [["uint256", "uint256", "uint256"]], [[...arguments]]);
+  constructor(ratioState, checkTarget, targetRatio, vaultId, nextPrice) {
+    super("McdRatioCheck", getAddr("McdRatioCheck"), [["uint8","bool","uint256","uint256","uint256"]], [[...arguments]]);
 
     this.mappableArgs = [
       this.args[0][0],
-      this.args[0][1],
       this.args[0][2],
+      this.args[0][3],
+      this.args[0][4],
     ];
   }
 
 }
 
 module.exports = MakerRatioCheckAction;
+
