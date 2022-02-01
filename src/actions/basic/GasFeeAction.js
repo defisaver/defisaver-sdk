c3923918bae67a12b6c5e61b114180603bd0ab14
Nenad Palinkasevic <nesa993@gmail.com>
2022-02-01 11:17:45 +0100

    Update fee taking action

--- src/actions/basic/GasFeeAction.js
+++ src/actions/basic/GasFeeAction.js
@@ -6,13 +6,19 @@ class GasFeeAction extends Action {
    * @param gasStart {string} Always 0 will be inject value
    * @param feeToken {string} Address of the token we are taken the fee in
    * @param availableAmount Amount we have available to pay the gas fee
+   * @param dfsFeeDivider Additional fee for DFS, default is 5bps
    */
-  constructor(gasStart, feeToken, availableAmount) {
-    super("GasFeeTaker", getAddr("GasFeeTaker"), [["uint256","address","uint256"]], [[...arguments]]);
+  constructor(gasStart, feeToken, availableAmount, dfsFeeDivider = 2000) {
+    super("GasFeeTaker",
+      getAddr("GasFeeTaker"),
+      [["uint256", "address", "uint256", "uint256"]],
+      [[gasStart, feeToken, availableAmount, dfsFeeDivider]],
+    );
 
     this.mappableArgs = [
       this.args[0][1],
       this.args[0][2],
+      this.args[0][3],
     ];
   }
 
