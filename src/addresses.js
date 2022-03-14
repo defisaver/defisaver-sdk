3ccb3b62e529c4e342ac3de5dd2eaad8af978c07
Nenad Palinkasevic <nesa993@gmail.com>
2022-03-14 16:18:41 +0100

    Add curve deposit/withdraw addresses

--- src/addresses.js
+++ src/addresses.js
@@ -124,6 +124,9 @@ const actionAddresses = {
    'MStableWithdrawNew': '0x6468215Bf4811244Ab78599e1df9206716A35aB7',
 
    'MStableClaim': '0x28279A806aDeDedFD33e39C7375dc0c0ee943847',
+  
+   'CurveStethPoolDeposit': '0x0f042a5CC97C4cd8D8136c28F966839f195D08B9',
+   'CurveStethPoolWithdraw': '0x2F4ad71Bd8045d0633B304BD2d94ef349eB09105',
 };
 
 const otherAddresses = {
