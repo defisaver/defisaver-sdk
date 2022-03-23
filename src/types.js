c0d79b35a9ecec0da86af697d90c98d29a3be136
cussone <aleksandar.cakalic@gmail.com>
2022-03-23 13:22:12 +0100

    Add new types

--- src/types.js
+++ src/types.js
@@ -4,6 +4,11 @@
  */
 
 /**
+ * Chain ID number of the Network
+ * @typedef {number} chainId
+ */
+
+/**
  * Maker vault ID
  * @typedef {(string|number)} VaultId
  */
@@ -17,4 +22,32 @@
 
 /**
  * @typedef {Array<AccessListItem>} AccessList
+ */
+
+/**
+ * Global configuration object
+ * @typedef {Object.<string, any>} Config
+ * @property {chainId}
+ */
+
+/**
+ * @typedef {object} NativeCurrency
+ * @property {string} name
+ * @property {string} symbol
+ * @property {number} decimals
+ */
+
+/**
+ * @typedef {object} Network
+ * @property {chainId} chainId
+ * @property {string} chainName
+ * @property {array<string>} blockExplorerUrls
+ * @property {array<string>} iconUrls
+ * @property {array<string>} rpcUrls
+ * @property {NativeCurrency} nativeCurrency
+ */
+
+/**
+ * @typedef Networks
+ * @type {Object.<string, Network>}
  */
