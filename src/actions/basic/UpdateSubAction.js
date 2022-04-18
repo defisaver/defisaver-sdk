2844aabbbc1aa38ab4012ba92d8c0ba5932e2011
Nenad Palinkasevic <nesa993@gmail.com>
2022-04-18 10:51:12 +0200

    remove addr as first param in updatesub action

--- src/actions/basic/UpdateSubAction.js
+++ src/actions/basic/UpdateSubAction.js
@@ -9,7 +9,7 @@ class UpdateSubAction extends Action {
    * @param subId id of the subscription in the SubStorage contract
    * @param sub object that contains new sub information
    */
-  constructor(storageAddress, subId, sub) {
+  constructor(subId, sub) {
     super("UpdateSub", getAddr("UpdateSub"), [["uint256", "(uint64,bool,bytes[],bytes32[])"]], [[...arguments]]);
 
   }
