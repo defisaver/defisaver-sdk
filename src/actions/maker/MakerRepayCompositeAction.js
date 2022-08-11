98b6c43d34a2603f482b6a6827ca1d5db9d22e3e
kjaredic <kjaredic@protonmail.com>
2022-08-11 14:07:56 +0200

    update mcd composite actions with gasUsed argument

--- src/actions/maker/MakerRepayCompositeAction.js
+++ src/actions/maker/MakerRepayCompositeAction.js
@@ -9,12 +9,14 @@ class MakerRepayCompositeAction extends Action {
      * @param vaultId {VaultId}
      * @param mcdManager {EthAddress}
      * @param joinAddr {EthAddress}
+     * @param gasUsed {string}
      * @param exchangeParams {ExchangeData}
      */
     constructor(
         vaultId,
         mcdManager,
         joinAddr,
+        gasUsed,
         exchangeParams,
     ) {
         super(
@@ -24,12 +26,14 @@ class MakerRepayCompositeAction extends Action {
                 'uint256',
                 'address',
                 'address',
+                'uint256',
                 ['address', 'address', 'uint256', 'uint256', 'uint256', 'uint256', 'address', 'address', 'bytes', ['address', 'address', 'address', 'uint256', 'uint256', 'bytes']],
             ],
             [
                 vaultId,
                 mcdManager,
                 joinAddr,
+                gasUsed,
                 exchangeParams,
             ],
         );
@@ -38,9 +42,9 @@ class MakerRepayCompositeAction extends Action {
             this.args[0],
             this.args[1],
             this.args[2],
-            this.args[3][1],
-            this.args[3][2],
-            this.args[3][0],
+            this.args[4][1],
+            this.args[4][2],
+            this.args[4][0],
           ];
     }
 }
