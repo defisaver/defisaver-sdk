b5af02e8bf32612cc8c8b85e97410031f5ab1084
majkic99 <vrnebojsamajkic@gmail.com>
2022-09-13 12:35:05 +0200

    change RecipeExecutor address mainnet

--- src/addresses.js
+++ src/addresses.js
@@ -212,7 +212,7 @@ const actionAddresses = {
 
 const otherAddresses = {
   [NETWORKS.ethereum.chainId]: {
-    RecipeExecutor: '0xe822d76c2632FC52f3eaa686bDA9Cea3212579D8',
+    RecipeExecutor: '0x1D6DEdb49AF91A11B5C5F34954FD3E8cC4f03A86',
     DFSRegistry: '0x287778F121F134C66212FB16c9b53eC991D32f5b',
     DFSProxyRegistry: '0x29474FdaC7142f9aB7773B8e38264FA15E3805ed',
     ProxyRegistry: '0x4678f0a6958e4D2Bc4F1BAF7Bc52E8F3564f3fE4',
