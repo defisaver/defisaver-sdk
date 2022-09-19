3fc4b13afa44e6432bff5b4ebe9db9bd662dc373
Nenad Palinkasevic <nesa993@gmail.com>
2022-09-19 16:45:53 +0200

    Add CompV3RatioTrigger

--- src/triggers/index.js
+++ src/triggers/index.js
@@ -7,7 +7,7 @@ const CompoundRatioTrigger = require('./CompoundRatioTrigger');
 const ReflexerRatioTrigger = require('./ReflexerRatioTrigger');
 const LiquityRatioTrigger = require('./LiquityRatioTrigger');
 const AaveV3RatioTrigger = require('./AaveV3RatioTrigger');
-const CompV3RatioTrigger = require('./AaveV3RatioTrigger');
+const CompV3RatioTrigger = require('./CompV3RatioTrigger');
 
 module.exports = {
     MakerRatioTrigger,
