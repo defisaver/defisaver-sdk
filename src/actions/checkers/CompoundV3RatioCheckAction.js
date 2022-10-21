b85fe3ccbf79ca882d8251b4d881bd10c470a3a3
Nenad Palinkasevic <nesa993@gmail.com>
2022-10-21 12:28:11 +0200

    Update CompV3RatioChecker

--- src/actions/checkers/CompoundV3RatioCheckAction.js
+++ src/actions/checkers/CompoundV3RatioCheckAction.js
@@ -2,21 +2,23 @@ const Action = require("../../Action");
 const {getAddr} = require("../../addresses.js");
 
 /**
- * CompoundV3RatioCheckAction - Checks comp V3 ratio for users proxy position and reverts if faulty
+ * CompoundV3RatioCheckAction - Checks comp V3 ratio for user position and reverts if faulty
  */
 class CompoundV3RatioCheckAction extends Action {
   /**
    * @param ratioState {uint8} If it should lower/higher
    * @param targetRatio {string} The ratio user want to be at
    * @param market {address} Address of compV3 market
+   * @param user {address} Address of the user we are checking the ratio for (default to proxy)
    */
   constructor(ratioState, targetRatio, market) {
-    super("CompV3RatioCheck", getAddr("CompV3RatioCheck"), ["uint8","uint256","address"], [ratioState, targetRatio, market]);
+    super("CompV3RatioCheck", getAddr("CompV3RatioCheck"), ["uint8","uint256","address","address"], [ratioState, targetRatio, market, user]);
 
     this.mappableArgs = [
       this.args[0],
       this.args[1],
-      this.args[2]
+      this.args[2],
+      this.args[3]
     ];
   }
 
