69550efbb659fbc2a6aaf98f8b14923cff1cfa36
Nenad Palinkasevic <nesa993@gmail.com>
2022-11-01 13:13:01 +0100

    Add CBUpdateRebondSubAction

---
+++ src/actions/chickenBonds/CBUpdateRebondSubAction.js
@@ -0,0 +1,19 @@
+const Action = require("../../Action");
+const { requireAddress } = require("../../utils/general");
+const { getAddr } = require('../../addresses.js');
+
+/**
+ * CBUpdateRebondSubAction - Updates rebond strategy subscription
+ */
+class CBUpdateRebondSubAction extends Action {
+  /**
+   * @param subId {string} Id of the subscription to update
+   * @param bondId {string} Id of the bond in the strategy
+   */
+  constructor(subId, bondId) {
+    super('CBUpdateRebondSub', getAddr('CBUpdateRebondSub'), ['uint256','uint256'], [...arguments]);
+  }
+
+}
+
+module.exports = CBUpdateRebondSubAction;
--- src/actions/chickenBonds/index.js
+++ src/actions/chickenBonds/index.js
@@ -2,10 +2,12 @@ const CBCreateAction = require('./CBCreateAction');
 const CBChickenInAction = require('./CBChickenInAction');
 const CBChickenOutAction = require('./CBChickenOutAction');
 const CBRedeemAction = require('./CBRedeemAction');
+const CBUpdateRebondSubAction = require('./CBUpdateRebondSubAction');
 
 module.exports = {
   CBCreateAction,
   CBChickenInAction,
   CBChickenOutAction,
   CBRedeemAction,
+  CBUpdateRebondSubAction,
 };
