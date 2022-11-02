73ac832cae564b6b7597861c93a3931aa9e1c6af
MatijaPojatar <58627531+MatijaPojatar@users.noreply.github.com>
2022-11-02 14:55:50 +0100

    Export ts Actions

---
+++ src/actions/index.ts
@@ -0,0 +1,49 @@
+import maker from './maker';
+import aave from './aave';
+import compound from './compound';
+import basic from './basic';
+import flashloan from './flashloan';
+import uniswap from './uniswap';
+import reflexer from './reflexer';
+import dydx from './dydx';
+import uniswapV3 from './uniswapV3';
+import checkers from './checkers';
+import liquity from './liquity';
+import yearn from './yearn';
+import lido from './lido';
+import insta from './insta';
+import balancer from './balancer';
+import curve from './curve';
+import guni from './guni';
+import mstable from './mstable';
+import rari from './rari';
+import aaveV3 from './aaveV3';
+import convex from './convex';
+import chickenBonds from './chickenBonds';
+import compoundV3 from './compoundV3';
+
+export default {
+  maker,
+  aave, // Aave v2
+  compound,
+  basic,
+  flashloan,
+  uniswap, // Uniswap v2
+  uniswapV3,
+  reflexer,
+  dydx,
+  checkers,
+  liquity,
+  yearn,
+  lido,
+  insta,
+  balancer,
+  curve,
+  guni,
+  mstable,
+  rari,
+  aaveV3,
+  convex,
+  chickenBonds,
+  compoundV3,
+};
