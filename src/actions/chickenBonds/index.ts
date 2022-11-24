012db42085b71c67ed67ab2445351598c687f9b8
majkic99 <vrnebojsamajkic@gmail.com>
2022-11-24 14:09:20 +0700

    add to index.ts

--- src/actions/chickenBonds/index.ts
+++ src/actions/chickenBonds/index.ts
@@ -2,3 +2,5 @@ export * from './CBCreateAction';
 export * from './CBChickenInAction';
 export * from './CBChickenOutAction';
 export * from './CBRedeemAction';
+export * from './CBUpdateRebondSubAction';
+export * from './FetchBondIdAction';
