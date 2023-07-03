2fd4973c10cacd28ef7ce8b9032e1fcc39d3df28
Nenad Palinkasevic <nesa993@gmail.com>
2023-07-03 14:46:15 +0200

    Remove sfrxEth market

--- src/utils/curveusd-utils.ts
+++ src/utils/curveusd-utils.ts
@@ -1,13 +1,11 @@
 /// @dev debtAvailableBlock is only used in v3-contracts repo
 export const curveusdMarkets = {
-  sfrxETH: { controllerAddress: '0x8472a9a7632b173c8cf3a86d3afec50c35548e76', debtAvailableBlock: 17425859 },
   wstETH: { controllerAddress: '0x100dAa78fC509Db39Ef7D04DE0c1ABD299f4C6CE', debtAvailableBlock: 17487165 },
   WBTC: { controllerAddress: '0x4e59541306910ad6dc1dac0ac9dfb29bd9f15c67', debtAvailableBlock: 17563176 },
   WETH: { controllerAddress: '0xa920de414ea4ab66b97da1bfe9e6eca7d4219635', debtAvailableBlock: 17563176 },
 };
 
 export const controllerToAssetMap = {
-  '0x8472a9a7632b173c8cf3a86d3afec50c35548e76': '0xac3E018457B222d93114458476f3E3416Abbe38F',
   '0x100dAa78fC509Db39Ef7D04DE0c1ABD299f4C6CE': '0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0',
   '0x4e59541306910ad6dc1dac0ac9dfb29bd9f15c67': '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',
   '0xa920de414ea4ab66b97da1bfe9e6eca7d4219635': '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
