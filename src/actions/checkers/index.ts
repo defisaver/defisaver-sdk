d44fbcbbcef89f8213ee7e6d0b75c8c04b78ce5e
Nenad Palinkasevic <nesa993@gmail.com>
2023-08-01 15:30:21 +0200

    Add missing actions for aaveV2 and compV2 automation

--- src/actions/checkers/index.ts
+++ src/actions/checkers/index.ts
@@ -2,3 +2,5 @@ export * from './MakerRatioCheckAction';
 export * from './AaveV3RatioCheckAction';
 export * from './CompoundV3RatioCheckAction';
 export * from './LiquityRatioCheckAction';
+export * from './AaveV2RatioCheckAction';
+export * from './CompoundV2RatioCheckAction';
