5f2fc77a43de876d505f4ea0315afd1271516d9b
majkic99 <vrnebojsamajkic@gmail.com>
2024-04-10 11:32:08 +0200

    Update LlamaLendBoostAction.ts

--- src/actions/llamalend/LlamaLendBoostAction.ts
+++ src/actions/llamalend/LlamaLendBoostAction.ts
@@ -7,21 +7,19 @@ import { EthAddress, uint256, uint32 } from '../../types';
  * @category LlamaLend
  */
 export class LlamaLendBoostAction extends Action {
-  constructor(controller: EthAddress, exchangeOrder: Array<any>, to: EthAddress, gasUsed: uint32) {
+  constructor(controller: EthAddress, exchangeOrder: Array<any>, gasUsed: uint32) {
     super(
       'LlamaLendBoost',
       getAddr('LlamaLendBoost'),
       [
         'address',
         ['address', 'address', 'uint256', 'uint256', 'uint256', 'uint256', 'address', 'address', 'bytes', ['address', 'address', 'address', 'uint256', 'uint256', 'bytes']],
-        'address',
         'uint32',
       ],
-      [controller, exchangeOrder, to, gasUsed],
+      [controller, exchangeOrder, gasUsed],
     );
     this.mappableArgs = [
       this.args[0],
-      this.args[2],
     ];
   }
 }
