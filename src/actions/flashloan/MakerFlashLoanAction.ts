ab0509e9e5291fd28d7642769c12c9ded08d04ff
Aleksandar Milosavljević <alemilosav@gmail.com>
2024-05-15 14:58:25 +0200

    fix: remove dependency from constructor name for identifing specific FL action

--- src/actions/flashloan/MakerFlashLoanAction.ts
+++ src/actions/flashloan/MakerFlashLoanAction.ts
@@ -1,19 +1,22 @@
 import { Action } from '../../Action';
 import { getAddr } from '../../addresses';
 import { EthAddress, uint256, bytes } from '../../types';
+import { FlashLoanId } from './FlashLoanId';
 
 /**
  * Gets a flashloan from Maker
  *
  * @category Flashloans
  */
-export class MakerFlashLoanAction extends Action {
+export class MakerFlashLoanAction extends Action implements FlashLoanId {
+  public flashLoanId = 4;
+
   /**
    * @param amount
    * @param flParamGetterAddr
    * @param flParamGetterData
    */
-  constructor(amount:uint256, flParamGetterAddr:EthAddress = getAddr('Empty'), flParamGetterData:bytes = []) {
+  constructor(amount: uint256, flParamGetterAddr: EthAddress = getAddr('Empty'), flParamGetterData: bytes = []) {
     super(
       'FLMaker',
       getAddr('FLMaker'),
