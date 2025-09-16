c393ddc9188db2253eb4fa7eb3996ac61da33972
Milos Djurica <milosdjurica.work@gmail.com>
2025-09-16 16:23:13 +0200

    feat: update AaveV3RatioCheck actions to accept extra user & market params

--- src/actions/checkers/AaveV3RatioCheckAction.ts
+++ src/actions/checkers/AaveV3RatioCheckAction.ts
@@ -1,6 +1,6 @@
 import { Action } from '../../Action';
 import { getAddr } from '../../addresses';
-import { uint8, uint256 } from '../../types';
+import { uint8, uint256, EthAddress } from '../../types';
 
 /**
  * AaveV3RatioCheckAction - Checks aave V3 ratio for users proxy position and reverts if faulty
@@ -11,13 +11,27 @@ export class AaveV3RatioCheckAction extends Action {
   /**
    * @param ratioState If it should lower/higher
    * @param targetRatio The ratio user want to be at
+   * @param market Address provider for specific market. This param was added later
+   * @param user Address of the user (EOA/SW). This param was added later
    */
-  constructor(ratioState:uint8, targetRatio:uint256) {
-    super('AaveV3RatioCheck', getAddr('AaveV3RatioCheck'), ['uint8', 'uint256'], [ratioState, targetRatio]);
+  constructor(
+    ratioState: uint8,
+    targetRatio: uint256,
+    market: EthAddress = getAddr('Empty'),
+    user: EthAddress = getAddr('Empty'),
+  ) {
+    super(
+      'AaveV3RatioCheck',
+      getAddr('Empty'), // TODO -> is this ok?
+      ['uint8', 'uint256', 'address', 'address'],
+      [ratioState, targetRatio, market, user],
+    );
 
     this.mappableArgs = [
       this.args[0],
       this.args[1],
+      this.args[2],
+      this.args[3],
     ];
   }
 }
