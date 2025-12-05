792820ed01466152d2763a3bdb30ae3c71aabf79
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2025-12-05 14:13:37 +0100

    fix: remove unused state field in AaveV4QuotePriceRangeTrigger

--- src/triggers/AaveV4QuotePriceRangeTrigger.ts
+++ src/triggers/AaveV4QuotePriceRangeTrigger.ts
@@ -1,6 +1,6 @@
 import { Action } from '../Action';
 import { getAddr } from '../addresses';
-import { EthAddress, uint256, uint8 } from '../types';
+import { EthAddress, uint256 } from '../types';
 
 /**
  * AaveV4QuotePriceRangeTrigger - Verifies if current token price ratio for aaveV4 spoke is within a subbed price range.
@@ -14,13 +14,12 @@ export class AaveV4QuotePriceRangeTrigger extends Action {
     quoteTokenId:uint256,
     lowerPrice:uint256,
     upperPrice:uint256,
-    state:uint8,
   ) {
     super(
       'AaveV4QuotePriceRangeTrigger',
       getAddr('Empty'),
-      [['address', 'uint256', 'uint256', 'uint256', 'uint256', 'uint8']],
-      [[spoke, baseTokenId, quoteTokenId, lowerPrice, upperPrice, state]],
+      [['address', 'uint256', 'uint256', 'uint256', 'uint256']],
+      [[spoke, baseTokenId, quoteTokenId, lowerPrice, upperPrice]],
     );
   }
 }
