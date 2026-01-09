441e90110137f938c676f24bce1787ad576118f7
marko-makaric <marko.makaric@decenter.com>
2026-01-09 16:22:54 +0100

    chore: [review] added market addr to target ratio check

--- src/actions/checkers/SparkTargetRatioCheck.ts
+++ src/actions/checkers/SparkTargetRatioCheck.ts
@@ -1,6 +1,6 @@
 import { Action } from '../../Action';
 import { getAddr } from '../../addresses';
-import { uint256 } from '../../types';
+import { EthAddress, uint256 } from '../../types';
 
 /**
  * SparkTargetRatioCheck - SparkRatioCheckAction - Checks spark ratio for users proxy position and reverts if faulty
@@ -10,12 +10,14 @@ import { uint256 } from '../../types';
 export class SparkTargetRatioCheck extends Action {
   /**
      * @param targetRatio The ratio user want to be at
+     * @param market Address of the market
      */
-  constructor(targetRatio: uint256) {
-    super('SparkTargetRatioCheck', getAddr('SparkTargetRatioCheck'), ['uint256'], [targetRatio]);
+  constructor(targetRatio: uint256, market: EthAddress) {
+    super('SparkTargetRatioCheck', getAddr('SparkTargetRatioCheck'), ['uint256', 'address'], [targetRatio, market]);
 
     this.mappableArgs = [
       this.args[0],
+      this.args[1],
     ];
   }
 }
