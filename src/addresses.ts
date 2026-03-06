44929dd90f363361323c10e0327277e962a7f581
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2026-03-06 13:57:02 +0100

    chore: add deployment addresses for sig actions

--- src/addresses.ts
+++ src/addresses.ts
@@ -397,6 +397,9 @@ export const actionAddresses = {
     AaveV4Payback: '0x6e31Dd331571209043c8CF997f86b4291F648537',
     AaveV4CollateralSwitch: '0x26C39FE05466dBA72A98d095d019dC5e067F6b28',
     AaveV4RefreshPremium: '0xb080DC160415Ffe1a4b80d75b0Be92EE38a0b426',
+    AaveV4SetUserManagersWithSig: '0xe3459a0fEBc8276c428951125A10f589ea195760',
+    AaveV4DelegateWithdrawWithSig: '0xDDCcE054Ea32dc857A2D7Cf87E55310Feb2DD86c',
+    AaveV4DelegateBorrowWithSig: '0xdDef60521E4cf82e971D1f1387ab0d5e226D0533',
   },
   [NETWORKS.optimism.chainId]: {
     DFSSell: '0x9f234af5c10c136863a20865ba00b26951ab8269',
